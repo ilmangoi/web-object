cf96da9e60b3fe6083930f4f6ae51b605082e656
ilmango <mangguoli42@gmail.com>
2022-06-19 18:54:26 +0800

    feat: add style lint

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-html/vue',
+    'stylelint-config-standard-scss',
+    'stylelint-config-recommended-vue/scss',
+    'stylelint-config-recess-order',
+    'stylelint-prettier/recommended'
+  ]
+}
--- README.md
+++ README.md
@@ -5,4 +5,3 @@
 | npm run lint     | 语法检查   |
 | npm run commit   | 提交项目   |
 | npm run prettier | 格式化风格 |
-
